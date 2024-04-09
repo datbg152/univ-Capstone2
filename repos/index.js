6711cc0cc9b3715049643ced88b34e6cff808383
deveb1479@gmail.com <deveb1479@gmail.com>
2024-04-09 14:24:34 +0900

    repository start

---
+++ repos/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    const _retfile = path.resolve(__dirname, 'public/repository.html');
+
+    res.sendFile(_retfile);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
