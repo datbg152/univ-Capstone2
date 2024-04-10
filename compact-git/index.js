38c3f98223f76dc3c66b74277bd75ed4b881fe17
elecbug <deveb1479@gmail.com>
2024-04-10 15:31:03 +0900

    hmm

--- compact-git/index.js
+++ compact-git/index.js
@@ -3,8 +3,17 @@ const app = express()
 const port = 3000
 const path = require('path')
 const qs = require('querystring')
+const mysql = require('mysql')
 
 app.use(express.static('public'))
+
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+// flush privileges;
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: "root",
+    password: "password"
+});
 
 app.get('/', (req, res) => {
     const retfile = path.resolve(__dirname, 'public/main.html')
@@ -18,7 +27,7 @@ app.post('/', (req, res) => {
             body += data
         })
         req.on('end', () => {
-            var post = qs.parse(body);
+            const post = qs.parse(body);
             
             if (post.type === 'type.login') {
                 res.redirect('/login')
@@ -45,15 +54,41 @@ app.post('/sign-up', (req, res) => {
             body += data
         })
         req.on('end', () => {
-            var post = qs.parse(body);
+            const post = qs.parse(body);
             
             if (post.type === 'type.sign-up-up') {
-                console.log('sign up')
+                var query = 'use user_db;'
+                con.query(query, (err, result) => {
+                    if (err) throw err;
+                    console.log("Result: " + result);
+                });
+                query = 'insert into user values("' + post.id + '", "' + post.pw + '", "' + post.name + '");';
+                con.query(query, (err, result) => {
+                    if (err) {
+                        if (err.errno == 1062) {
+                            res.send('Duplicate ID')
+                            return
+                        }
+                    }
+                    console.log("Result: " + result);
+                    res.redirect('/private')
+                });
             }
         });
     }
 })
 
+app.get('/private', (req, res) => {
+    const retfile = path.resolve(__dirname, 'public/private.html')
+    res.sendFile(retfile)
+})
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+con.connect((err) => {
+    if (err) throw err;
+    console.log("Connected!");
+});
