5e4337a69b72127af0b69ef3a7ca42b8c1bd4575
Pandamon <jaehong576@gmail.com>
2024-05-02 22:34:42 +0900

    commit

---
+++ studies/박재홍/My_Repo/My_repo.js
@@ -0,0 +1,29 @@
+function new_click()
+{
+
+}
+
+function type_click()
+{
+
+}
+
+function settings_click()
+{
+
+}
+
+function column_button()
+{
+
+}
+
+var New_button = document.getElementById("New");
+var Type_button = documnet.getElementById("Type");
+var Settings_button = documnet.getElementById("Settings");
+var Column_button = document.getElementById("column_button");
+
+New_button.addEventListener("click", new_click);
+Type_button.addEventListener("click", type_click);
+Settings_button.addEventListener("click", settings_click);
+Column_button.addEventListener("click", column_button);
