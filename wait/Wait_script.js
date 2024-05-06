09699388f17618c24304ba036e32c6d4d7c63f9e
elecbug <deveb1479@gmail.com>
2024-05-06 13:46:33 +0900

    del

--- wait/Wait_script.js
+++
@@ -1,6 +0,0 @@
-/*일정 시간 이후 버튼이 노출되게 하는 함수*/
-setTimeout(function()
-{
-    document.getElementById('func_button').style.display='block';
-    document.getElementById('func_message').style.display='block';
-}, 3000);
