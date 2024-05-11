eaa75cdc8bb945e3062e9d13553f78887f05f485
elecbug <deveb1479@gmail.com>
2024-05-11 20:58:46 +0900

    a

--- studies/쩐레민득/240326/views/community.js
+++
@@ -1,7 +0,0 @@
-export default () => `
-<button class="button back-button" data-back>
-<i class="fa fa-arrow-left"></i> back
-</button>
-
-<h1>Community</h1> 
-`;
--- studies/쩐레민득/240326/views/repository.js
+++
@@ -1,7 +0,0 @@
-export default () => /*html*/ `
-<button class="button back-button" data-back>
-<i class="fa fa-arrow-left"></i> back
-</button>
-
-<h1>Repository</h1>
-`;
